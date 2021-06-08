40c24256538146fc71381f88b99e75611680c984
KoleBarnes <kbarnes261@gmail.com>
2021-06-08 14:29:58 -0700

    * Fix pipe to file issue on Windows
    - Only use `winpty` when running interactively on Windows.
    - This fixes the `stdout is not a tty` issue seen on Windows when piping the output of the script to a file.
      - For example; `./run.sh --net smn --status > status.json`
    
    Removed all trailing white-space from network names.
    Added network name and consistent timestamp to node summary.
    
    - Add the network name to the node summary so monitoring results can be filtered and grouped by network.
    - Ensure there is a consistent (UTC) timestamp on the results that can be used as the time series for the collected data.  This ensures events can be compared across nodes regardless of any clock skew on the nodes themselves.
    
    Updated run script.
    
    - Add support for specifying the number of samples to collect via an environment variable (default 1); `SAMPLES`.
      - Samples are taken one minute apart as the Nodes refresh their own data every minute.
    - Add support for logging results directly to a timestamped file via an environment variable; `LOG`.
    
    Full Example:
    `SAMPLES=20 LOG=/c/tick-sandbox/telegraf/indy-node-monitor/BuilderNet/BuilderNet-AllNodes.json ./run.sh --net sbn --seed <seed>`
    - Runs the `fetch_status` process 20 times (once every minute) producing a set of files in `/c/tick-sandbox/telegraf/indy-node-monitor/BuilderNet/` with a timestamped filename that looks like this; `BuilderNet-AllNodes_2021-05-23_06-48-20.json`.
    
    Signed-off-by: KoleBarnes <kbarnes261@gmail.com>

--- fetch-validator-status/run.sh
+++ fetch-validator-status/run.sh
@@ -15,11 +15,16 @@ function getVolumeMount() {
   echo "  --volume='${path}:/home/indy/${mountPoint}:Z' "
 }
 
-# Running on Windows?
-if [[ "$OSTYPE" == "msys" ]]; then
-  # Prefix interactive terminal commands ...
-  terminalEmu="winpty"
-fi
+function runCmd() {
+  _cmd=${1}
+  if [ ! -z ${LOG} ]; then
+    _cmd+=" > ${LOG%.*}_`date +\%Y-\%m-\%d_%H-%M-%S`.json"
+  fi
+
+  eval ${_cmd}
+  # echo
+  # echo ${_cmd}
+}
 
 # IM is for "interactive mode" so Docker is run with the "-it" parameter. Probably never needed
 # but it is there. Use "IM=1 run.sh <args>..." to run the Docker container in interactive mode
@@ -27,6 +32,12 @@ if [ -z "${IM+x}" ]; then
   export DOCKER_INTERACTIVE=""
 else
   export DOCKER_INTERACTIVE="-it"
+  
+  # Running interactively on Windows?
+  if [[ "$OSTYPE" == "msys" ]]; then
+    # Prefix interactive terminal commands ...
+    export terminalEmu="winpty"
+  fi
 fi
 
 docker build -t fetch_status . > /dev/null 2>&1
@@ -35,7 +46,6 @@ cmd="${terminalEmu} docker run --rm ${DOCKER_INTERACTIVE} \
   -e "GENESIS_PATH=${GENESIS_PATH}" \
   -e "GENESIS_URL=${GENESIS_URL}" \
   -e "SEED=${SEED}""
-  
 
 # Dynamically mount teh 'conf' directory if it exists.
 if [ -d "./conf" ]; then
@@ -47,5 +57,15 @@ if [ -d "./plugins" ]; then
 fi
 
 cmd+="fetch_status \"$@\""
-eval ${cmd}
-#echo ${cmd}
+
+counter=${SAMPLES:-1}
+while [[ ${counter} > 0 ]]
+do
+  runCmd "${cmd}"
+  counter=$(( ${counter} - 1 ))
+  if [[ ${counter} > 0 ]]; then
+    # Nodes update their validator info every minute.
+    # Therefore calling more than once per minute is not productive.
+    sleep 60
+  fi
+done
